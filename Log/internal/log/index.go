dcf2208ffb64d60cc9d3e59821fc4d6bfb0839b7
Gavin Chan <gavinwhchan@gmail.com>
2020-10-11 21:48:02 -0700

    index read/write + tests

--- Log/internal/log/index.go
+++ Log/internal/log/index.go
@@ -73,15 +73,36 @@ func (i *index) Close() error {
 }
 
 // Takes in an offset, and returns the associated record's position in the store
+// The offset is relative to the segment's base offset
+// The offset is 4B because it saves space - significant once there are a lot of records
 func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 	if i.size == 0 {
 		return 0, 0, io.EOF
 	}
-
 	if in == -1 {
 		out = uint32((i.size / entWidth) - 1)
 	} else {
 		out = uint32(in)
 	}
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
+		return 0, 0, io.EOF
+	}
+	out = enc.Uint32(i.mmap[pos : pos+offWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
+	return out, pos, nil
+}
 
+func (i *index) Write(off uint32, pos uint64) error {
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
+	return nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
 }
