3fca499b3c7af27b8f513299d55121b63f734086
Gavin Chan <gavinwhchan@gmail.com>
2020-10-11 20:47:14 -0700

    newIndex and index close

---
+++ Log/internal/log/index.go
@@ -0,0 +1,87 @@
+package log
+
+import (
+	"io"
+	"os"
+
+	"github.com/tysontate/gommap"
+)
+
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File
+	mmap gommap.MMap
+	size uint64
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	idx.size = uint64(fi.Size())
+	// Grow the file to max index size for mmap.
+	// We can't resize it after we mmap the file.
+	// This will add some unknown amount of space
+	// between the last entry and the file's end.
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+
+	return idx, nil
+}
+
+func (i *index) Close() error {
+	// Syncs the changes made to this memory-mapped region to the persisted file
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+
+	// This calls the fsync syscall which will force the file system
+	// to flush it's buffers to disk. This guarantees that the data is on disk
+	// even if the system is powered down or the OS crashes.
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+
+	// Truncate the file that we grew in newIndex.
+	// This means removing the empty space between
+	// the max file size and the last entry. This will let
+	// the service find the last entry of the index when restarting.
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	return i.file.Close()
+}
+
+// Takes in an offset, and returns the associated record's position in the store
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+
+}
