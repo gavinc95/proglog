c562526d07c780e640f2839f0743aca626ecc2f1
Gavin Chan <gavinwhchan@gmail.com>
2020-10-24 20:32:57 -0700

    add gRPC to proto file and endpoints

--- Log/go.mod
+++ Log/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tysontate/gommap v0.0.0-20190103205956-899e1273fb5c
+	google.golang.org/grpc v1.33.1
 	launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
 )
---
+++ Log/internal/server/server.go
@@ -0,0 +1,90 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/gavinc95/proglog/api/v1"
+)
+
+// Guarantees the *grpcServer type satisfies the api.LogServer interface
+// Helpes others know what they can or can't change
+var _ api.LogServer = (*grpcServer)(nil)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+type grpcServer struct {
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ConsumeResponse{Record: record}, nil
+}
+
+// ProduceStream is a bi-directional streaming RPC.
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// ConsumeStream has server-side streaming - the client tells the server where in the log to read
+// and the server will stream with every following record, even ones that aren't in the log yet.
+// When it hits the end of the log, it waits for someone to append a new record and keeps streaming.
+func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_ConsumeStreamServer) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
